c1cbce06f2364a257262cac113404ebac6ecc3f6
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2022-12-23 15:44:06 -0300

    Rotas Filhas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,30 @@
 import { NgModule } from '@angular/core';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AlunosModule } from './alunos/alunos.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { CursosModule } from './cursos/cursos.module';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     AppRoutingModule,
-    CursosModule
+    CursosModule,
+    AlunosModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
