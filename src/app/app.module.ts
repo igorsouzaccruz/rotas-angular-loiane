4dd896d88527854335a48d7106a04992fca675f7
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2022-12-23 19:03:02 -0300

    Rotas: Dica de Performance: Carregamento sob demanda (lazy loading)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AlunosModule } from './alunos/alunos.module';
+// import { AlunosModule } from './alunos/alunos.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
-import { CursosModule } from './cursos/cursos.module';
+// import { CursosModule } from './cursos/cursos.module';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -22,8 +22,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserAnimationsModule,
     MatToolbarModule,
     AppRoutingModule,
-    CursosModule,
-    AlunosModule,
+    // CursosModule,
+    // AlunosModule,
     NgbModule,
 
   ],
