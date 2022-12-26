075612fe5d75a299e0b64fc2d338ee9c3557f59a
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2022-12-26 11:07:18 -0300

    Usando Guarda de Rotas: CanDeactivate com Interface Genérica

--- src/app/alunos/aluno-form/aluno-form.component.ts
+++ src/app/alunos/aluno-form/aluno-form.component.ts
@@ -1,3 +1,4 @@
+import { IFormCanDeactivate } from './../../guards/iform-candeactivate';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { combineLatest, Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
@@ -8,7 +9,7 @@ import { AlunosService } from '../alunos.service';
   templateUrl: './aluno-form.component.html',
   styleUrls: ['./aluno-form.component.css']
 })
-export class AlunoFormComponent implements OnInit, OnDestroy {
+export class AlunoFormComponent implements OnInit, OnDestroy, IFormCanDeactivate {
 
   aluno: any = {};
   inscricao: Subscription | undefined;
@@ -46,6 +47,10 @@ export class AlunoFormComponent implements OnInit, OnDestroy {
       confirm('Tem certeza que deseja sair dessa página');
     }
     return true;
+  }
+
+  podeDesativar() {
+    return this.podeMudarRota();
   }
 
 }
--- src/app/guards/alunos-deactivate.guard.ts
+++ src/app/guards/alunos-deactivate.guard.ts
@@ -1,17 +1,19 @@
+import { IFormCanDeactivate } from './iform-candeactivate';
 import { AlunoFormComponent } from './../alunos/aluno-form/aluno-form.component';
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot } from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable, retry } from "rxjs";
 
 
 @Injectable({providedIn: 'root'})
-export class AlunosDeactivateGuard implements CanDeactivate<AlunoFormComponent> {
+export class AlunosDeactivateGuard implements CanDeactivate<IFormCanDeactivate> {
   canDeactivate(
-    component: AlunoFormComponent,
+    component: IFormCanDeactivate,
     currentRoute: ActivatedRouteSnapshot,
     currentState: RouterStateSnapshot
   ): Observable<boolean>|boolean {
 
-    return !component.podeMudarRota();
+    // return !component.podeMudarRota();
+    return component.podeDesativar();
   }
 }
